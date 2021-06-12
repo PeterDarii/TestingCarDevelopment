01995d24e350e26308b329a3c38eab0adbfb7bc7
PeterDarii <peterdarii27@gmail.com>
2021-06-12 10:18:37 -0700

    final red audi car at your service

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,12 @@ public class Main {
     public static void main(String[] args) {
 
         car audi=new car();
+        audi.color="Red";
+        audi.number="777";
+        audi.doors="3";
+        audi.model="A3";
+        
+
 
 
     }
